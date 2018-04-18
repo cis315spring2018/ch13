0fc9147841d6bbc0a0baf9f841529eb544dcdac7
atmanning <atmanning@WS10TC11101.dbq.int>
2018-04-18 09:57:48 -0500

    initial commit

---
+++ chapter13Recursion/src/chapter13Recursion/JFrameMain.java
@@ -0,0 +1,112 @@
+package chapter13Recursion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Label;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class JFrameMain extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldInput;
+	private JTextField textFieldEx1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameMain frame = new JFrameMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameMain() {
+		setTitle("Recursion Practice");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 939, 645);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textFieldInput = new JTextField();
+		textFieldInput.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldInput.setText("5");
+		textFieldInput.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		textFieldInput.setBounds(23, 11, 198, 56);
+		contentPane.add(textFieldInput);
+		textFieldInput.setColumns(10);
+
+		
+		Label labelAnswer = new Label("See answer here");
+		labelAnswer.setBackground(Color.LIGHT_GRAY);
+		labelAnswer.setFont(new Font("Dialog", Font.PLAIN, 26));
+		labelAnswer.setAlignment(Label.CENTER);
+		labelAnswer.setBounds(445, 11, 444, 56);
+		contentPane.add(labelAnswer);
+		
+		
+		JButton btnFactorial = new JButton("factorial");
+		btnFactorial.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//  Button clicked - calculate Factorial!
+				
+				// calculate value, then show int the Label
+				double n = Double.parseDouble(textFieldInput.getText());
+				double answer = Recursion.factorial(n);
+				
+				// show the answer
+				labelAnswer.setText("" + answer);
+			}
+		});
+		
+
+		btnFactorial.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		btnFactorial.setBounds(245, 11, 176, 56);
+		contentPane.add(btnFactorial);
+		
+		JButton button = new JButton("13-1");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		button.setBounds(245, 101, 176, 56);
+		contentPane.add(button);
+		
+		textFieldEx1 = new JTextField();
+		textFieldEx1.setText("5");
+		textFieldEx1.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldEx1.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		textFieldEx1.setColumns(10);
+		textFieldEx1.setBounds(23, 101, 198, 56);
+		contentPane.add(textFieldEx1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(445, 101, 444, 230);
+		contentPane.add(scrollPane);
+		
+		JTextArea txtAnswer13_1 = new JTextArea();
+		txtAnswer13_1.setText("Answer13_1");
+		scrollPane.setViewportView(txtAnswer13_1);
+	}
+}
---
+++ chapter13Recursion/src/chapter13Recursion/Recursion.java
@@ -0,0 +1,41 @@
+package chapter13Recursion;
+
+public class Recursion {
+
+	// This is a utility class for defining my recursive functions
+	// See Malik book chapter 13
+	
+	public static double factorial( double n ) {
+		
+		if( n <= 0 )  // Base case
+			return 1;
+		
+		// recursion!  
+		return n * factorial( n-1 );
+	}
+	
+	// Chapter 13 - Exercise 1
+	public String strWedge(int n) {
+		
+
+		String sResult = "";
+		
+		if( 0 == n )  // end Case
+			return "";
+	
+		// general case
+		for(int i=0; i<n; i++)  // n stars
+			sResult += "*";
+		sResult += "\n";  // end of line
+		
+		sResult += strWedge(n-1);  // recursion here!
+		
+		for(int i=0; i<n; i++) // n stars
+			sResult += "*";
+		sResult += "\n";  // end of line	
+		
+		return sResult;
+	}
+	
+	
+}
